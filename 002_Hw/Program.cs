9b19a82bd2d89bc442cb3670a2f296b5688a6cd9
Nadin <nmsmet@ya.ru>
2022-07-22 14:59:40 +0300

    Add folder 002_Hw

---
+++ 002_Hw/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов 
+//в промежутке от M до N.
+
+
+int f(int n, int m)
+{
+    if(n == m)
+    return n;
+
+    return f(n-1, m) + n;
+}
+
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());;
+
+Console.WriteLine(f(n, m));
+Console.WriteLine();
