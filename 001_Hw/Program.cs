62c01e604368cb9f1f7b3fdbf8b46b222ab134a2
Nadin <nmsmet@ya.ru>
2022-07-22 14:59:19 +0300

    Add folder 001_Hw

---
+++ 001_Hw/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа 
+//в промежутке от M до N.
+
+
+void f(int n, int m)
+{
+    if(n != (m-1))
+    {
+        f(n-1, m);
+        Console.Write(n + " ");
+
+    }
+}
+
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());;
+
+f(n, m);
+Console.WriteLine();
